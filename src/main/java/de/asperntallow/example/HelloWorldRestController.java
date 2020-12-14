c30ed7a41e296f5032b02f2df47b07035273c23e
adrian.weber <adrian.weber@msg.group>
2020-12-14 12:12:45 +0100

    feat: hello world accepts name parameter
    The hello world rest controller accepts now a name parameter to greet individual persons.
    
    Refs: IFS-42

--- src/main/java/de/asperntallow/example/HelloWorldRestController.java
+++ src/main/java/de/asperntallow/example/HelloWorldRestController.java
@@ -1,14 +1,15 @@
 package de.asperntallow.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldRestController {
 
     @GetMapping("/hello-world")
-    public String helloWorld() {
-        return "Hello, World!";
+    public String helloWorld(@RequestParam("name") String name) {
+        return String.format("Hello, %s!", name);
     }
 
 }
